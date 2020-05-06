5c56e93a1e67437e6659e0132eb8859a639ac2e8
lxy764139720 <764139720@qq.com>
2020-05-06 21:11:24 +0800

    confusion matrix

--- ecg.py
+++ ecg.py
@@ -1,34 +1,22 @@
+import os
 import datetime
+
 import wfdb
 import pywt
+import seaborn
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.InputLayer(input_shape=(300, 1)),
-    # 第一个卷积层, 4 个 21x1 卷积核
-    tf.keras.layers.Conv1D(filters=4, kernel_size=21, strides=1, padding='SAME', activation='relu'),
-    # 第一个池化层, 最大池化,4 个 3x1 卷积核, 步长为 2
-    tf.keras.layers.MaxPool1D(pool_size=3, strides=2, padding='SAME'),
-    # 第二个卷积层, 16 个 23x1 卷积核
-    tf.keras.layers.Conv1D(filters=16, kernel_size=23, strides=1, padding='SAME', activation='relu'),
-    # 第二个池化层, 最大池化,4 个 3x1 卷积核, 步长为 2
-    tf.keras.layers.MaxPool1D(pool_size=3, strides=2, padding='SAME'),
-    # 第三个卷积层, 32 个 25x1 卷积核
-    tf.keras.layers.Conv1D(filters=32, kernel_size=25, strides=1, padding='SAME', activation='relu'),
-    # 第三个池化层, 平均池化,4 个 3x1 卷积核, 步长为 2
-    tf.keras.layers.AvgPool1D(pool_size=3, strides=2, padding='SAME'),
-    # 第四个卷积层, 64 个 27x1 卷积核
-    tf.keras.layers.Conv1D(filters=64, kernel_size=27, strides=1, padding='SAME', activation='relu'),
-    # 打平层,方便全连接层处理
-    tf.keras.layers.Flatten(),
-    # 全连接层,128 个节点
-    tf.keras.layers.Dense(128, activation='relu'),
-    # Dropout层,dropout = 0.2
-    tf.keras.layers.Dropout(rate=0.2),
-    # 全连接层,5 个节点
-    tf.keras.layers.Dense(5, activation='softmax')
-])
+# 项目目录
+project_path = "D:\\python\\mit-bih_ecg_recognition\\"
+# 定义日志目录,必须是启动web应用时指定目录的子目录,建议使用日期时间作为子目录名
+log_dir = project_path + "logs\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+model_path = project_path + "ecg_model.h5"
+
+# 测试集在数据集中所占的比例
+RATIO = 0.3
 
 
 # 读取心电数据和对应标签,并对数据进行小波去噪
@@ -59,7 +47,7 @@ def getDataSet(number, X_data, Y_data):
     rdata = pywt.waverec(coeffs=coeffs, wavelet='db5')
 
     # 获取心电数据记录中R波的位置和对应的标签
-    annotation = wfdb.rdann('../ecg_data/' + number, 'atr')
+    annotation = wfdb.rdann('ecg_data/' + number, 'atr')
     Rlocation = annotation.sample
     Rclass = annotation.symbol
 
@@ -84,6 +72,7 @@ def getDataSet(number, X_data, Y_data):
     return
 
 
+# 加载数据集并进行预处理
 def loadData():
     numberSet = ['100', '101', '103', '105', '106', '107', '108', '109', '111', '112', '113', '114', '115',
                  '116', '117', '119', '121', '122', '123', '124', '200', '201', '202', '203', '205', '208',
@@ -100,26 +89,92 @@ def loadData():
     train_ds = np.hstack((dataSet, lableSet))
     np.random.shuffle(train_ds)
 
-    # 返回数据集集和标签集
+    # 数据集及其标签集
     X = train_ds[:, :300].reshape(-1, 300, 1)
     Y = train_ds[:, 300]
-    return X, Y
+
+    # 测试集及其标签集
+    shuffle_index = np.random.permutation(len(X))
+    test_length = int(RATIO * len(shuffle_index))
+    test_index = shuffle_index[:test_length]
+    X_test, Y_test = X[test_index], Y[test_index]
+    return X, Y, X_test, Y_test
+
+
+# 构建CNN模型
+def buildModel():
+    newModel = tf.keras.models.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(300, 1)),
+        # 第一个卷积层, 4 个 21x1 卷积核
+        tf.keras.layers.Conv1D(filters=4, kernel_size=21, strides=1, padding='SAME', activation='relu'),
+        # 第一个池化层, 最大池化,4 个 3x1 卷积核, 步长为 2
+        tf.keras.layers.MaxPool1D(pool_size=3, strides=2, padding='SAME'),
+        # 第二个卷积层, 16 个 23x1 卷积核
+        tf.keras.layers.Conv1D(filters=16, kernel_size=23, strides=1, padding='SAME', activation='relu'),
+        # 第二个池化层, 最大池化,4 个 3x1 卷积核, 步长为 2
+        tf.keras.layers.MaxPool1D(pool_size=3, strides=2, padding='SAME'),
+        # 第三个卷积层, 32 个 25x1 卷积核
+        tf.keras.layers.Conv1D(filters=32, kernel_size=25, strides=1, padding='SAME', activation='relu'),
+        # 第三个池化层, 平均池化,4 个 3x1 卷积核, 步长为 2
+        tf.keras.layers.AvgPool1D(pool_size=3, strides=2, padding='SAME'),
+        # 第四个卷积层, 64 个 27x1 卷积核
+        tf.keras.layers.Conv1D(filters=64, kernel_size=27, strides=1, padding='SAME', activation='relu'),
+        # 打平层,方便全连接层处理
+        tf.keras.layers.Flatten(),
+        # 全连接层,128 个节点
+        tf.keras.layers.Dense(128, activation='relu'),
+        # Dropout层,dropout = 0.2
+        tf.keras.layers.Dropout(rate=0.2),
+        # 全连接层,5 个节点
+        tf.keras.layers.Dense(5, activation='softmax')
+    ])
+    return newModel
+
+
+# 混淆矩阵
+def plotHeatMap(Y_test, Y_pred):
+    con_mat = confusion_matrix(Y_test, Y_pred)
+    # 归一化
+    # con_mat_norm = con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis]
+    # con_mat_norm = np.around(con_mat_norm, decimals=2)
+
+    # 绘图
+    plt.figure(figsize=(8, 8))
+    seaborn.heatmap(con_mat, annot=True, fmt='.20g', cmap='Blues')
+    plt.ylim(0, 5)
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.show()
 
 
 def main():
-    X, Y = loadData()
-    # 定义日志目录,必须是启动web应用时指定目录的子目录,建议使用日期时间作为子目录名
-    log_dir = "D:/python/mit-bih_ecg_prediction/logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    # 定义TensorBoard对象
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    # X,Y为所有的数据集和标签集
+    # X_test,Y_test为拆分的测试集和标签集
+    X, Y, X_test, Y_test = loadData()
 
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    model.fit(X, Y, epochs=50,
-              batch_size=128,
-              validation_split=0.3,
-              callbacks=[tensorboard_callback])
+    if os.path.exists(model_path):
+        # 导入训练好的模型
+        model = tf.keras.models.load_model(filepath=model_path)
+    else:
+        # 构建CNN模型
+        model = buildModel()
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        model.summary()
+        # 定义TensorBoard对象
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        # 训练与验证
+        model.fit(X, Y, epochs=30,
+                  batch_size=128,
+                  validation_split=RATIO,
+                  callbacks=[tensorboard_callback])
+        model.save(filepath=model_path)
+
+    # 预测
+    Y_pred = model.predict_classes(X_test)
+    # 绘制混淆矩阵
+    plotHeatMap(Y_test, Y_pred)
 
 
 if __name__ == '__main__':
